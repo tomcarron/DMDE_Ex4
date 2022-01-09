23545e11abf464927d19b0733a2247bf109be17c
tomcarron <tcarron@tcd.ie>
2022-01-09 13:30:09 +0100

    Added problem 2 a and b

--- DMDE4.py
+++ DMDE4.py
@@ -42,7 +42,66 @@ plt.figure(0)
 plt.semilogx(redshift,sub_ang(redshift))
 plt.xlabel('z')
 plt.ylabel('$\Theta_{sc}$ [$^\circ$]')
-plt.savefig('BAO_angle_z.png',dpi=400,bbox_inches='tight')
+plt.savefig('plots/BAO_angle_z.png',dpi=400,bbox_inches='tight')
+
+
+'''
+Problem 2: Cluster Luminosity (8 points)
+For this problem, use data from clusters.txt, which you can find on eCampus. The sample
+of clusters in this file is flux limited such that fX (0.1 − 2.4 keV) ≥ 5 × 10−12 erg s−1 cm−2 .
+The given fX limit in the 0.1-2.4 keV energy band corresponds to our (the observer’s) rest
+frame, while the LX values correspond to the 0.1-2.4 keV energy band in the cluster’s rest
+frame.
+'''
+file=open("clusters.txt","r")
+
+lines=file.readlines()
+name=[]
+z=[]
+flux=[]
+Lx=[]
+for x in lines[1:]:
+    name.append(x.split('\t')[0])
+    z.append(float(x.split('\t')[1]))
+    flux.append(float(x.split('\t')[2]))
+    Lx.append(float(x.split('\t')[3]))
+file.close()
+print(Lx)
+#print(z)
+
+'''
+a) Using the same cosmology parameters as the previous question, plot the X-ray lumi-
+nosity LX vs z. (3 points)
+'''
+
+
+
+'''
+On the plot above, add the flux limit line (the lowest LX that can be observed for every
+z based on the given flux limit). Make sure to show the equation you use to plot this
+limit line. (3 points)
+'''
+def Lum_lim(z):
+    fx=5e-12 #erg/s/cm2
+    dl=cosmo.luminosity_distance(z).to(u.cm)
+    Lx=fx*4*np.pi*(dl**2)
+    return Lx
+
+z2=np.linspace(0,0.7,1000)
+
+plt.figure(1)
+plt.scatter(z,Lx,s=1)
+plt.plot(z2,Lum_lim(z2),'red',linestyle='--')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('z')
+plt.ylabel('Lx')
+plt.ylim(1e41,1e46)
+plt.savefig('plots/xray_lum.png',dpi=400,bbox_inches='tight')
+
+'''
+c) Do all the clusters appear above the flux limit line? Why is this the case? (2 points)
+'''
 plt.show()
 
 

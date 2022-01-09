457bf3cea2f86ebd11137679f232aac2256f5b7e
tomcarron <tcarron@tcd.ie>
2022-01-09 10:59:24 +0100

    Added emptyish script

---
+++ DMDE4.py
@@ -0,0 +1,9 @@
+#Script for DMDE Ex4
+import numpy as np
+import astropy
+import astropy.constants as const
+import astropy.units as u
+import matplotlib.pyplot as plt
+from astropy.cosmology import Planck18_arXiv_v2 as cosmo
+import pandas as pd
+
